23cf700d1bead078afb014ff07198a3acbde1c68
Banchan Sangma <bsangma32@gmail.com>
2020-06-12 19:59:32 +0530

    Added message when word is not found

--- helpers.js
+++ helpers.js
@@ -9,12 +9,21 @@ exports.translateToGaro = (word) => {
     crlfDelay: Infinity,
   });
   
+  let notFound = true;
+
   rl.on('line', (line) => {
     [english, garo] = line.split('=');
     if( english === word ) {
       console.log(`${english}: ${garo}`);
+      notFound = false;
       rl.close();
       readStream.destroy();
+    }
+  });
+
+  rl.on('close', () => {
+    if(notFound) {
+      console.error(`${word} was not found in the dictionary.`);
     }
   });
 };
